2067c09ff28390476ddcc3761c207294d8593da9
张杰 <1700117425@qq.com>
2018-09-05 12:48:50 +0800

    fix

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import xlrd
 from keys import keys_list_new
 import xlwt
+import sys
 from xlutils.copy import copy
-
+import os
 
 def check(s1, s2):
     return sum(map(lambda ch: s1.count(ch), s2))
@@ -36,7 +37,7 @@ def load_excel(filename):
     for i in range(0, sheet.nrows):
         article = {}
         article['title'] = sheet.cell(i, 0).value
-        article['content'] = sheet.cell(i, 7).value
+        article['content'] = str(sheet.cell(i, 7).value)
         article_list.append(article)
     return article_list
 
@@ -51,15 +52,18 @@ def classify(article_list, keys_list):
 
     # 存放分类结果（id:类目的二级id；title：类目的名称；keys：类目包含的所有关键词；keys_sort:筛选出靠前的关键词供填入表格中）
     result_list = []
-
+    print(len(article_list))
     # 遍历文章列表
     for article in article_list:
-
         # 最大匹配数标志
         max_num = 0
         # 当前文章最佳分类的结果
         result = {}
         # 最佳分类的关键词及匹l'l
+        max_words=[]
+        for keys in keys_list:
+            num=0
+            words=[]
             for key in keys['keys']:
                 temp = {}
                 temp_num = 0
@@ -73,8 +77,11 @@ def classify(article_list, keys_list):
                 max_words = words
         # print('--------------')
                 max_num = num
-                result = keys
+                result = keys.copy()
+
         new_max_words = sort_keys(max_words)
+        if len(new_max_words) is 0:
+            print('空')
         result['words'] = new_max_words
         # print(result)
         # print('出现频率：', max_num)
@@ -97,7 +104,7 @@ def sort_keys(words):
     for word in words:
         new_word = {}
         # 如果该关键词匹配数大于1，则放入匹配结果关键词列表中
-        if word['num'] > 1:
+        if word['num'] >= 1:
             new_word['num'] = word['num']
             new_word['word'] = word['word']
             new_words.append(new_word)
@@ -110,8 +117,7 @@ def write_result(file, zt_list):
     nrows = ExcelFile.sheet_by_index(0).nrows
     new_excel = copy(ExcelFile)
     sheet = new_excel.get_sheet(0)
-    i = 1
-    for i in range(1, nrows):
+    for i in range(0, nrows):
         try:
             sheet.write(i, 3, str(zt_list[i]['id'])+zt_list[i]['title'])
             word_text = ""
@@ -120,13 +126,20 @@ def write_result(file, zt_list):
             sheet.write(i, 6, word_text)
             i += 1
         except Exception as e:
-            print(e)
-            pass
-    new_excel.save('new.xls')
+            s = sys.exc_info()
+            print("Error '%s' happened on line %d" % (s[1], s[2].tb_lineno))
+    new_excel.save(file)
 
 
 if __name__ == '__main__':
-    article_list = load_excel('C:/excel/信息公开目录.xls')
-    # keys_list_ = load_key('C:/excel/数据.xls')
-    result_list = classify(article_list, keys_list_new)
-    write_result('C:/excel/信息公开目录.xls', result_list)
+    for path in os.listdir('C:/excel/'):
+        if path.split('.')[-1] == 'xls':
+            article_list = load_excel('C:/excel/'+path)
+            # keys_list_ = load_key('C:/excel/数据.xls')
+            result_list = classify(article_list, keys_list_new)
+            write_result('C:/excel/'+path, result_list)
+
+    # article_list = load_excel('C:/excel/信息公开目录.xls')
+    # # keys_list_ = load_key('C:/excel/数据.xls')
+    # result_list = classify(article_list, keys_list_new)
+    # write_result('C:/excel/信息公开目录.xls', result_list)
